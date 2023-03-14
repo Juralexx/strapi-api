96bbc2b7f2aafdf0138e6ceda00fdd1cbf748765
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-03-14 18:50:28 +0100

    Added custom 'find' response

--- src/api/introduction/controllers/introduction.ts
+++ src/api/introduction/controllers/introduction.ts
@@ -3,5 +3,17 @@
  */
 
 import { factories } from '@strapi/strapi'
+import { Introduction } from '../../../types';
 
-export default factories.createCoreController('api::introduction.introduction');
+export default factories.createCoreController('api::introduction.introduction', ({ strapi }) => ({
+    async find(ctx) {
+        const { data, meta } = await super.find(ctx)
+
+        let datas: Introduction.Type[] = []
+
+        const { id, attributes } = data
+        datas = { id: id, ...attributes }
+
+        return { data: datas, meta };
+    },
+}));
