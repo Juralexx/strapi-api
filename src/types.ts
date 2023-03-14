54b7fe83a6d7e2f5932c7af320c3657c2904aa1b
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-03-14 18:50:49 +0100

    Updated namespaces

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,66 @@
+interface ContentBase {
+    createdBy?: Record<string, any> | string,
+    createdAt?: Record<string, any> | string,
+    updatedBy?: Record<string, any> | string,
+    updatedAt?: Record<string, any> | string,
+    publishedBy?: Record<string, any> | string,
+    publishedAt?: Record<string, any> | string
+}
+
+export namespace Introduction {
+    export type defaultType = {
+        id: number,
+        attributes: IAttributes
+    }
+
+    export interface IAttributes extends ContentBase {
+        content: string
+    }
+
+    export interface Type extends ContentBase {
+        id: number,
+        content: string
+    }
+}
+
+export namespace Checkerboard {
+    export type defaultType = {
+        id: number,
+        attributes: IAttributes
+    }
+
+    export interface IAttributes extends ContentBase {
+        title: string,
+        content: string
+    }
+
+    export interface Type extends ContentBase {
+        id: number,
+        title: string,
+        content: string
+    }
+}
+
 export namespace Actuality {
-    export type Type = {
+    export type defaultType = {
+        id: number,
+        attributes: IAttributes
+    }
+
+    export interface IAttributes extends ContentBase {
         title: string,
         content: string,
         date: string,
-        image?: number | string,
-        gallery?: any,
-        updatedBy?: number | string,
-        updatedAt?: string
-        createddAt?: string
+        image?: Record<string, any>,
+        gallery?: any
+    }
+
+    export interface Type extends ContentBase {
+        id: number,
+        title: string,
+        content: string,
+        date: string,
+        image?: Record<string, any>,
+        gallery?: any
     }
 }
