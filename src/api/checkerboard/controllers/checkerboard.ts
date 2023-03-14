22068091bffd4b54d3b84fd24bc430562f1c245a
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-03-14 18:49:40 +0100

    Added custom 'find' response

--- src/api/checkerboard/controllers/checkerboard.ts
+++ src/api/checkerboard/controllers/checkerboard.ts
@@ -3,5 +3,19 @@
  */
 
 import { factories } from '@strapi/strapi'
+import { Checkerboard } from '../../../types';
 
-export default factories.createCoreController('api::checkerboard.checkerboard');
+export default factories.createCoreController('api::checkerboard.checkerboard', ({ strapi }) => ({
+    async find(ctx) {
+        const { data, meta } = await super.find(ctx)
+
+        let datas: Checkerboard.Type[] = []
+
+        data.forEach((item: Checkerboard.defaultType, i: number) => {
+            const { id, attributes } = item
+            datas = [...datas, { id: id, ...attributes }]
+        })
+
+        return { data: datas, meta };
+    },
+}));
