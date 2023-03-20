2b62d5bff47b7d5a37858306402b401c5e0f1bbd
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-03-20 16:23:16 +0100

    Update date check on create and update

--- src/api/actuality/content-types/actuality/lifecycles.ts
+++ src/api/actuality/content-types/actuality/lifecycles.ts
@@ -1,37 +1,65 @@
 import { Actuality } from "../../../../types";
 import { convertStringToURL, numericDateParser } from "../../../../utils";
+import { ValidationError } from '@strapi/utils/lib/errors'
 
-const getUrlOnCreate = (data: Actuality.IAttributes) => {
-    const titleToUrl = convertStringToURL(data.title)
-    let datefr: string | string[] = numericDateParser(data.date)
-    const regExp = new RegExp('/', "g")
-    datefr = datefr.replace(regExp, '-')
-    datefr = datefr.split('-')
-    const date = `${datefr[0]}-${datefr[1]}-${datefr[2]}`
-    return `${date}/${titleToUrl}`
-}
-
-const getUrlOnUpdate = (data: Actuality.IAttributes) => {
-    const titleToUrl = convertStringToURL(data.title)
-    const datefr = data.date.split('-')
-    const date = `${datefr[2]}-${datefr[1]}-${datefr[0]}`
+const getUrl = (data: Actuality.IAttributes) => {
+    const titleToUrl: string = convertStringToURL(data.title)
+    const datefr: string[] = data.date.split('-')
+    const date: string = `${datefr[2]}-${datefr[1]}-${datefr[0]}`
     return `${date}/${titleToUrl}`
 }
 
 export default {
     async beforeCreate(event: any) {
         const { data } = event.params;
+        const regExp: RegExp = new RegExp('/', "g")
         if (!data.date) {
-            data.date = new Date();
+            let today = numericDateParser(new Date().toString());
+            today = today.replace(regExp, '-')
+            let arr = today.split('-')
+            data.date = `${arr[2]}-${arr[1]}-${arr[0]}`
         }
-        const url = getUrlOnCreate(data)
+        if (data.date.includes('/')) {
+            data.date = data.date.replace(regExp, '-')
+        }
+        const url = getUrl(data)
         data.url = url
+
+        const entries = await strapi.entityService.findMany('api::actuality.actuality', {
+            populate: 'deep',
+        })
+
+        entries.forEach((entry: Actuality.Type) => {
+            if (entry.title === data.title && entry.date === data.date) {
+                throw new ValidationError('Une actualité possédant le même titre existe déjà à cette date. Deux actualités ne peuvent pas posséder la même date et le même titre.');
+            }
+        })
     },
     async beforeUpdate(event: any) {
         const { data } = event.params;
+        const regExp: RegExp = new RegExp('/', "g")
+        if (!data.date) {
+            let today = numericDateParser(new Date().toString());
+            today = today.replace(regExp, '-')
+            let arr = today.split('-')
+            data.date = `${arr[2]}-${arr[1]}-${arr[0]}`
+        }
+        if (data.date.includes('/')) {
+            data.date = data.date.replace(regExp, '-')
+        }
         if (data.title) {
-            const url = getUrlOnUpdate(data)
+            const url = getUrl(data)
             data.url = url
         }
+
+        const entries = await strapi.entityService.findMany('api::actuality.actuality', {
+            populate: 'deep',
+        })
+
+        entries.forEach((entry: Actuality.Type) => {
+            if (entry.title === data.title && entry.date === data.date) {
+                throw new ValidationError('Une actualité possèdant le même titre existe déjà à cette date. Deux actualités ne peuvent pas posséder la même date et le même titre.');
+            }
+        })
     },
 }
