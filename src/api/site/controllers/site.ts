45c325ae80fbc164eceda9651aea6c3f36a25cf5
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-03-22 17:36:50 +0100

    updated controller

--- src/api/site/controllers/site.ts
+++ src/api/site/controllers/site.ts
@@ -10,8 +10,14 @@ export default factories.createCoreController('api::site.site', ({ strapi }) => 
         const entry = await strapi.db.query('api::site.site').findOne({
             populate: ['deep']
         })
+        const navbar = await strapi.db.query('api::navigation.navigation').findOne({
+            populate: ['deep']
+        })
+        const infos = await strapi.db.query('api::informations-menu.informations-menu').findOne({
+            populate: ['deep']
+        })
 
-        let datas: Site.Type[] = entry
+        let datas: Site.Type[] = { ...entry, ...navbar, ...infos }
 
         return { ...datas };
     },
