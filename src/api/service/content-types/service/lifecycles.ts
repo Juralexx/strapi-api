d57a909f7aa098763462260b3213903505b68611
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-04-04 04:44:40 +0200

    Updated lifecycles.js

--- src/api/service/content-types/service/lifecycles.ts
+++ src/api/service/content-types/service/lifecycles.ts
@@ -16,15 +16,17 @@ export default {
         })
     },
     async beforeUpdate(event: any) {
-        const { data } = event.params;
+        const { data, where } = event.params;
 
         const entries = await strapi.entityService.findMany('api::service.service', {
             populate: 'deep',
         })
 
         entries.forEach((entry: Service.Type) => {
-            if (entry.url === data.url) {
-                throw new ValidationError('Une page est déjà reliée à l\'URL spécifiée.');
+            if (entry.id !== where.id) {
+                if (entry.url === data.url) {
+                    throw new ValidationError('Une actualité possèdant le même titre existe déjà à cette date. Deux actualités ne peuvent pas posséder la même date et le même titre.');
+                }
             }
         })
     },
